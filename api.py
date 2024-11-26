664f7e328bf1ed7db32c004ba01626b2d155cc20
jean binde <jean-francois binde1@ulaval.ca>
2024-11-25 19:54:18 -0500

    fifth commit

--- api.py
+++ api.py
@@ -5,7 +5,6 @@ URL = 'https://pax.ulaval.ca/quixo/api/a24/'
 
 
 def initialiser_partie(idul, secret):
-    """Initialise une nouvelle partie."""
     rep = requests.post(
         URL + 'partie/',
         auth=(idul, secret)
@@ -34,6 +33,22 @@ def jouer_un_coup(id_partie, origine, direction, idul, secret):
         data = rep.json()
         if data['gagnant']:
             raise StopIteration(data['gagnant'])
+        return data['id'], data['état']['joueurs'], data['état']['plateau']
+    elif rep.status_code == 401:
+        raise PermissionError(rep.json()['message'])
+    elif rep.status_code == 406:
+        raise RuntimeError(rep.json()['message'])
+    else:
+        raise ConnectionError()
+
+
+def obtenir_etat_partie(id_partie, idul, secret):
+    rep = requests.get(
+        f"{URL}partie/{id_partie}/",
+        auth=(idul, secret)
+    )
+    if rep.status_code == 200:
+        data = rep.json()
         return data['id'], data['état']['joueurs'], data['état']['plateau']
     elif rep.status_code == 401:
         raise PermissionError(rep.json()['message'])
